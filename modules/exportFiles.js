5a85e37a0614b9d6b2e6b87c9698978124f46075
kk <a631953720@gmail.com>
2021-07-15 21:56:17 +0800

    feat: 新增可切換輸出檔案的內容

--- modules/exportFiles.js
+++ modules/exportFiles.js
@@ -9,18 +9,13 @@ const {
     INIT_MONEY,
     WIN_BONUS_ARR,
     LOSE_BONUS_ARR,
-    DEBUG_MODE
+    DEBUG_MODE,
+    OUTPUT_TYPE
 } = process.env;
 
 function exportXlsx(params) {
+    const testTable = outputType(params);
     const testData = [
-        ['區間內容', '次數'],
-        ['遊戲結束的現金 是初始現金的0.5倍以下的次數', params.a1],
-        ['遊戲結束的現金 是初始現金的0.5~1倍的次數', params.a2],
-        ['遊戲結束的現金 是初始現金的1~1.5倍的次數', params.a3],
-        ['遊戲結束的現金 是初始現金的1.5~2倍的次數', params.a4],
-        ['遊戲結束的現金 是初始現金的2倍以上的次數', params.a5],
-        [],
         ['參數名稱','參數設定'],
         ['賭金變動幅度',INTERVAL],
         ['每次遊戲的賭博次數',PLAY_TIMES],
@@ -32,7 +27,7 @@ function exportXlsx(params) {
         ['失敗賠錢參數陣列',LOSE_BONUS_ARR],
     ];
 
-    let arrayWorkSheet = xlsx.utils.aoa_to_sheet(testData);
+    let arrayWorkSheet = xlsx.utils.aoa_to_sheet(testTable.concat(testData));
     let workBook = {
         SheetNames: ['arrayWorkSheet'],
         Sheets: {
@@ -41,6 +36,32 @@ function exportXlsx(params) {
     };
 
     xlsx.writeFile(workBook, `./test-${Date.now()}.xlsx`);
+}
+
+function outputType (params) {
+    if (OUTPUT_TYPE === "money") {
+        return [
+            ['區間內容', '次數'],
+            ['遊戲結束的現金 是初始現金的0.5倍以下的次數', params.a1],
+            ['遊戲結束的現金 是初始現金的0.5~1倍的次數', params.a2],
+            ['遊戲結束的現金 是初始現金的1~1.5倍的次數', params.a3],
+            ['遊戲結束的現金 是初始現金的1.5~2倍的次數', params.a4],
+            ['遊戲結束的現金 是初始現金的2倍以上的次數', params.a5],
+            []
+        ]
+    }
+
+    if(OUTPUT_TYPE === "rateOfReturn"){
+        return [
+            ['區間內容', '次數'],
+            ['報酬率小於-50', params.a1],
+            ['報酬率介於-50~0', params.a2],
+            ['報酬率介於0~50', params.a3],
+            ['報酬率介於50~100', params.a4],
+            ['報酬率大於100', params.a5],
+            []
+        ]
+    }
 }
 
 module.exports = exportXlsx;
